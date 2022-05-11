7bf4c4d3621d6de74dd85a9623b91665a2817d97
Nathaly Colcha <natyliz_1792@hotmail.com>
2022-05-10 21:07:36 -0500

    ultimos cambios del dia 10052022

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,7 +1,10 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 export const Navbar = () => {
+    const navigate = useNavigate();
     const handleLogout = () =>{
-        console.log('logout!!!');
+        navigate('/login', {
+            replace: true
+        });
 
     }
     return (
@@ -18,14 +21,14 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink 
-                        className="nav-item nav-link" 
+                        className={( {isActive} ) => 'nav-item nav-link ' + (isActive ? 'active' :'') } 
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        className="nav-item nav-link" 
+                        className={( {isActive} ) => 'nav-item nav-link ' + (isActive ? 'active' :'') }
                         to="/dc"
                     >
                         DC
@@ -33,10 +36,10 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-info">
-                        Nathaly Bella
+                        Nathaly
                     </span>
                     <button 
                         className="nav-item nav-link btn"
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { DcScreen } from '../components/dc/DcScreen';
+//import { DcScreen } from '../components/dc/DcScreen';
 import { LoginScreen } from '../components/login/LoginScreen';
-import { MarvelScreen } from '../components/marvel/MarvelScreen';
-import { SearchScreen } from '../components/search/SearchScreen';
+//import { MarvelScreen } from '../components/marvel/MarvelScreen';
+//import { SearchScreen } from '../components/search/SearchScreen';
 import { Navbar } from '../components/ui/Navbar';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-        <Navbar />
         <Routes>
-            <Route path="/" element={<MarvelScreen />} />
-            <Route path="/marvel" element={<MarvelScreen />} />
-            <Route path="/dc" element={<DcScreen />} />
             <Route path="/login" element={<LoginScreen />} />
-            <Route path="/search" element={<SearchScreen />} />
+            <Route path="/*" element={<DashboardRoutes />} />
         </Routes>
     </BrowserRouter>
   )
